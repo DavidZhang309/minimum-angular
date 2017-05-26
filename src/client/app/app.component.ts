f258b5d345c29404678b011c46569b1fc70e3f15
David Zhang <david_zhang_309@hotmail.com>
2017-05-25 22:19:37 -0400

    Added github link

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1><a href="https://github.com/DavidZhang309/minimum-angular">Github</a>`,
 })
 export class AppComponent  { name = 'Angular'; }
